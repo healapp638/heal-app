61cfa86dd951298a42b9bb1829a51590433f0eaa
Punit Asija <punitasija6@gmail.com>
2026-04-17 18:52:36 +0530

    change in design

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import { PageTransition } from '@/components/animations';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="min-h-screen scroll-smooth relative backgroundImage">
+        <div className="min-h-screen bg-cream! scroll-smooth relative backgroundImage">
             <PageTransition>
                 {children}
             </PageTransition>
--- src/components/commonCard/AuthCard.tsx
+++ src/components/commonCard/AuthCard.tsx
@@ -7,7 +7,7 @@ const AuthCard = ({ children }: { children: ReactNode }) => {
         <div className='flex items-center  justify-center min-h-screen w-full p-4 '>
             <div className='w-full max-w-xl border-2 border-cream! rounded-2xl mx-auto'>
                 <ScaleIn duration={0.5}>
-                    <div className="bg-cream! rounded-lg">
+                    <div className="bg-cream! shadow-2xl shadow-cream! border-2 border-white! rounded-lg">
                         <div className='rounded-lg py-4 flex justify-center'>
                             <Image src={'/images/logo.png'} height={240} alt="SachTech Logo" className='w-full!' preview={false} draggable={false} />
                         </div>
