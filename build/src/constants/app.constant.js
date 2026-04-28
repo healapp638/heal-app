787aea17db65602b5752001ae562a25c628413a3
sachin sangwan <sachin.sangwan@sachtechsolution.com>
2026-04-28 13:42:01 +0530

    update

--- build/src/constants/app.constant.js
+++ build/src/constants/app.constant.js
@@ -32,8 +32,8 @@ let AGORA_CREDENTIAL;
 let AWS_CREDENTIAL;
 let STRIPE_CREDENTIAL;
 const APP = {
-    ACCESS_EXPIRY: "30m",
-    REFRESH_EXPIRY: "30d",
+    ACCESS_EXPIRY: "2m",
+    REFRESH_EXPIRY: "4m",
     PORT: process.env.PORT || 8000,
     API_PREFIX: process.env.API_PREFIX || "/api/v1",
     FRONTEND_URL: process.env.FRONTEND_URL || '',
--- src/constants/app.constant.ts
+++ src/constants/app.constant.ts
@@ -30,8 +30,8 @@ let AWS_CREDENTIAL: AwsCredential
 let STRIPE_CREDENTIAL: StripeCredential
 
 const APP: AppConstant = {
-  ACCESS_EXPIRY: "30m",
-  REFRESH_EXPIRY: "30d",
+  ACCESS_EXPIRY: "2m",
+  REFRESH_EXPIRY: "4m",
   PORT: process.env.PORT || 8000,
   API_PREFIX: process.env.API_PREFIX || "/api/v1",
   FRONTEND_URL: process.env.FRONTEND_URL || '',
