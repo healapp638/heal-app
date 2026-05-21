3bf937fc87913ec8dfb45b20bfcd1386268c0306
sachin sangwan <sachin.sangwan@sachtechsolution.com>
2026-05-21 14:56:00 +0530

    update

--- build/src/helpers/langauge.translate.helper.js
+++ build/src/helpers/langauge.translate.helper.js
@@ -8,30 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserTranslateText = exports.translateText = void 0;
 const translate_1 = require("@google-cloud/translate");
-const p_limit_1 = __importDefault(require("p-limit"));
 const translate = new translate_1.Translate({
     key: 'AIzaSyDo-P-1y_BlFP_amUtSxcFtksN_OTkxpi4' // Replace with your actual API key
 });
-const translateLimit = (0, p_limit_1.default)(10);
 const translateText = (text, targetLanguage) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         if (targetLanguage == 'en') {
             return text;
         }
-        return translateLimit(() => __awaiter(void 0, void 0, void 0, function* () {
-            const [translation] = yield translate.translate(text, {
-                from: 'en', // Source language
-                to: targetLanguage, // Target language
-                format: 'html', // Format if necessary
-            });
-            return translation; // Return the translated text
-        }));
+        const [translation] = yield translate.translate(text, {
+            from: 'en', // Source language
+            to: targetLanguage, // Target language
+            format: 'html', // Format if necessary
+        });
+        return translation; // Return the translated text
     }
     catch (errors) {
         console.log(errors);
--- src/helpers/langauge.translate.helper.ts
+++ src/helpers/langauge.translate.helper.ts
@@ -1,25 +1,22 @@
 import { Translate } from '@google-cloud/translate';
-import pLimit from 'p-limit';
 
 const translate = new Translate({
     key: 'AIzaSyDo-P-1y_BlFP_amUtSxcFtksN_OTkxpi4' // Replace with your actual API key
 });
 
-const translateLimit = pLimit(10);
+
 
 export const translateText = async (text: string, targetLanguage: string) => {
     try {
         if (targetLanguage == 'en') {
             return text
         }
-        return translateLimit(async () => {
-            const [translation] = await translate.translate(text, {
-                from: 'en', // Source language
-                to: targetLanguage, // Target language
-                format: 'html', // Format if necessary
-            });
-            return translation; // Return the translated text
-        })
+        const [translation] = await translate.translate(text, {
+            from: 'en', // Source language
+            to: targetLanguage, // Target language
+            format: 'html', // Format if necessary
+        });
+        return translation; // Return the translated text
     } catch (errors) {
         console.log(errors)
         return text
