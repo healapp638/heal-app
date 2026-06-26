9e71d0bee69958135152fb42eda4da509864232e
Bhawana Singh <bhawana.singh@sachtechsolution.com>
2026-06-26 14:08:20 +0530

    changes

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /var/www/html/Heal-Prod-API
+cd /var/www/html/Heal-Dev-API
 npm i -f
--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -3,24 +3,24 @@
 set -e
 
 echo "Stopping old process..."
-pm2 delete heal_api_3000 || true
+pm2 delete heal_api_3001 || true
 pm2 delete excel_import_worker || true
 pm2 delete bullMqWorker || true
 
 echo "Starting application..."
-cd /var/www/html/Heal-Prod-API
+cd /var/www/html/Heal-Dev-API
 
-npm run start:prod
+npm run start:dev
 
 echo "Waiting for app to start..."
 sleep 10
 
-echo "Checking if port 3000 is listening..."
+echo "Checking if port 3001 is listening..."
 
-if ss -tuln | grep -q ':3000'; then
-  echo "Application is running on port 3000"
+if ss -tuln | grep -q ':3001'; then
+  echo "Application is running on port 3001"
 else
-  echo "Application is not running on port 3000"
+  echo "Application is not running on port 3001"
 #  exit 1
 fi
 
